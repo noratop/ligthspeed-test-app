e146867692edca9c873de739a640737c621fa2f8
Nora <top.nora@gmail.com>
2016-03-05 21:44:48 -0500

    init contacttoolbar component

--- src/assets/js/components/ContactListView.jsx
+++ src/assets/js/components/ContactListView.jsx
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import Contact from './Contact';
+import ContactToolBar from './ContactToolBar';
 
 function ContactListView({list}){
   return (
     <div>
       <h2>Contacts</h2>
+      <ContactToolBar/>
       <ul>{list.map((contact)=>
           <Contact key={contact.id} contact={contact}/>
       )}</ul>
---
+++ src/assets/js/components/ContactToolBar.jsx
@@ -0,0 +1,16 @@
+import React, { Component, PropTypes } from 'react';
+
+class ContactToolBar extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {};
+   }
+
+  render(){
+    return (
+      <div>Tool bar</div>
+    )
+  }
+}
+
+export default ContactToolBar;
